2ca47af02fe544205b4526696c6f43be38813d6c
unknown <devops@pattesa.com>
2024-01-22 20:48:35 +0530

    security group

---
+++ terraform-modules/security_group.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "security_group" {
+  name        = "terraform-security-group"
+  description = "creating security group for terraform "
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow traffic from any source
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow SSH traffic from any source
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # Allow all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
